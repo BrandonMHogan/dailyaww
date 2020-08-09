91f2163efff61e9358e54edaf6ad6871fc95d68f
Brandon Hogan <brandon.michael.hogan@gmail.com>
2020-08-09 00:45:30 -0400

    fixed issue with out dated function in share service for streaming file from the cache

--- lib/services/share_service.dart
+++ lib/services/share_service.dart
@@ -2,10 +2,12 @@ import 'package:dailyaww/services/image_service.dart';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
 
 class ShareService {
+  /// shares an image using native share functionality.
+  /// will pull the image from the default cache stored in the image service.
   static void share(String title, String url, {String mimeType = 'image/jpg'}) {
-    ImageService.getCacheManager().getFile(url).first?.then((fileInfo) {
+    ImageService.getCacheManager().getSingleFile(url)?.then((fileInfo) {
       // converts the file to bytes and sends it off into the share service
-      fileInfo.file.readAsBytes().then((bytes) {
+      fileInfo.readAsBytes().then((bytes) {
         Share.file('The Daily Aww', title, bytes, mimeType,
             text: 'Shared From The Daily Aww app');
       });
