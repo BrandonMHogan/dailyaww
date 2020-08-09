98c53fa771d02533f007176b49d7448bd693231c
Brandon Hogan <brandon.michael.hogan@gmail.com>
2020-08-09 12:25:06 -0400

    added google service json and started setup of the analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:dailyaww/common/theme.dart';
 import 'package:dailyaww/features/detail/detail_widget.dart';
 import 'package:dailyaww/features/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 import 'common/routes.dart';
@@ -10,16 +13,59 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // uses material app style, and passes in the default widget
+  FirebaseAnalytics analytics = FirebaseAnalytics();
+  // final String name = 'Daily Aww';
+  // final FirebaseOptions options = const FirebaseOptions(
+  //   googleAppID: '1:315673900552:android:a508a1dda8e445790a03bd',
+  //   gcmSenderID: '315673900552',
+  //   apiKey: 'AIzaSyAO-qSUBYKqlM_a8sZLA6n2XR4vPZSOcYY',
+  // );
+
+  Widget app() {
     return MaterialApp(
       title: "The Daily Aww",
       theme: appTheme.theme,
       initialRoute: Routes.Home,
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
       routes: {
         Routes.Home: (context) => HomeWidget(),
         Routes.Detail: (context) => DetailWidget(),
+      },
+    );
+  }
+
+  Widget loading() {
+    return Dialog(
+      child: new Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          new CircularProgressIndicator(),
+          new Text("Loading"),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return app();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return app();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return loading();
       },
     );
   }
